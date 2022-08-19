1ca261ef9873aaf63aed4d0eca0537cafc0e2e76
池上拓 <takuron1996@gmail.com>
2022-08-19 18:52:10 +0900

    Update test_example.py

--- {{cookiecutter.project_name}}/tests/test_example.py
+++ {{cookiecutter.project_name}}/tests/test_example.py
@@ -7,14 +7,42 @@ __author__ = '{{cookiecutter.author}}'
 __version__ = '{{cookiecutter.version}}'
 __date__ = '{{cookiecutter.date}}'
 
-from test.support import captured_stdout
+import textwrap
+import os
+from test.support import captured_stdout, captured_stdin
 from {{cookiecutter.project_name}} import example
 
 
-def test_example():
-    """例題プログラムのテスト（標準出力）
+def test_example1():
+    """例題プログラムのテスト1
     """
-    with captured_stdout() as stdout:
+    input_value = textwrap.dedent("""\
+        72
+        128 256
+        myonmyon""")
+    output_value = textwrap.dedent("""\
+        456 myonmyon""")
+    run(input_value, output_value)
+
+
+def test_example2():
+    """例題プログラムのテスト2
+    """
+    input_value = textwrap.dedent("""\
+        1
+        2 3
+        test""")
+    output_value = textwrap.dedent("""\
+        6 test""")
+    run(input_value, output_value)
+
+
+def run(input_value, output_value):
+    """テスト実行（標準入力、標準出力）
+    """
+    with captured_stdout() as stdout, captured_stdin() as stdin:
+        stdin.write(input_value)
+        stdin.seek(0)
         example.main()
         lines = stdout.getvalue().splitlines()
-    assert "hello" == lines[0]
+    assert output_value == os.linesep.join(lines)
